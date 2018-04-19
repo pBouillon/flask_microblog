32c82dac53904c14d4c0caf88033acc942ec4cf1
pBouillon <pro.pierre.bouillon@protonmail.com>
2018-04-19 15:20:17 +0200

    Added login form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -20,3 +21,9 @@ def index():
         }
     ]
     return render_template('index.html', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
