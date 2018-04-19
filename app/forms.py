6c504a0ee9b4524861d79986d605dd89caf3f853
pBouillon <pro.pierre.bouillon@protonmail.com>
2018-04-19 16:55:30 +0200

    Added user register

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,14 @@ from wtforms import PasswordField
 from wtforms import BooleanField
 from wtforms import SubmitField
 
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import EqualTo
+from wtforms.validators import Email
+
+from app.models import User
+
+
+PASSWORD_MIN_LEN = 8
 
 
 class LoginForm(FlaskForm):
@@ -19,3 +26,50 @@ class LoginForm(FlaskForm):
     )
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        'Username',
+        validators=[DataRequired()]
+    )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Email()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()]
+    )
+    password2 = PasswordField(
+        'Repeat Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password')
+        ]
+    )
+    submit = SubmitField('Register')
+
+    def validate_username(self, username) -> None:
+        user = User.query.filter_by(
+            username=username.data
+        ).first()
+
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email) -> None:
+        # TODO implements check -> https://haveibeenpwned.com
+        user = User.query.filter_by(
+            email=email.data
+        ).first()
+
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+    def validate_password(self, password) -> None:
+        # TODO implements check -> https://haveibeenpwned.com
+        password_value = password.data
+
+        if len(password_value) < PASSWORD_MIN_LEN:
+            raise ValidationError('Password must be at least 8 characters')
--- app/routes.py
+++ app/routes.py
@@ -10,8 +10,8 @@ from flask_login import logout_user
 from flask_login import login_required
 from werkzeug.urls import url_parse
 
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -79,3 +79,45 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         user = User(username=form.username.data, email=form.email.data)
+#         user.set_password(form.password.data)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash('Congratulations, you are now a registered user!')
+#         return redirect(url_for('login'))
+#     return render_template('register.html', title='Register', form=form)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # redirect logged in users to home page
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+    if not form.validate_on_submit():
+        return render_template(
+            'register.html',
+            title='Register',
+            form=form
+        )
+    user = User(
+        username=form.username.data,
+        email=form.email.data
+    )
+    user.set_password(
+        form.password.data
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    flash('registered successfully !')
+
+    return redirect(url_for('login'))
+
