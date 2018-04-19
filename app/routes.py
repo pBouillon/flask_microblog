0cb881ea61fc14dd846324610705f3ed7c386ec7
pBouillon <pro.pierre.bouillon@protonmail.com>
2018-04-19 15:00:49 +0200

    First view ! (all mocked up)

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {
+        'username': 'pBouillon'
+    }
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Python is awesome !'
+        },
+        {
+            'author': {'username': 'Simon'},
+            'body': 'The cake is a lie.'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
