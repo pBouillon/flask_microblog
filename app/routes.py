ac33d5c945a502714bb33e4da4d98db7755427cb
pBouillon <pro.pierre.bouillon@protonmail.com>
2018-04-19 15:30:12 +0200

    Added redirection on submit

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 
 from app import app
 from app.forms import LoginForm
@@ -23,7 +23,21 @@ def index():
     return render_template('index.html', user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
+
+    if not form.validate_on_submit():
+        return render_template(
+            'login.html',
+            title='Sign In',
+            form=form
+        )
+
+    flash(
+        'Login requested for user {}, remember_me={}'.format(
+            form.username.data,
+            form.remember_me.data
+        )
+    )
+    return redirect('/index')
