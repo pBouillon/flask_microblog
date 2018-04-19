78fe6dd225d61387a7965012bceaaefbfc9a8423
pBouillon <pro.pierre.bouillon@protonmail.com>
2018-04-19 16:09:24 +0200

    Added db model

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
 from os import environ
+from os import path
+
+
+basedir = path.abspath(
+    path.dirname(__file__)
+)
 
 
 class Config:
     SECRET_KEY = environ.get('SECRET_KEY') or 'super-secret_key'
+
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or \
+        'sqlite:///' + path.join(
+            basedir,
+            'app.db)'
+        )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
